1ab8255b3aeb223db2cbcc95b6939c15ac6fe94b
Jim-CTChen <b07901036@ntu.edu.tw>
2021-01-20 02:23:11 +0800

    use module

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,5 @@
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
 const mongoose = require('mongoose');
 const express = require('express');
 const cors = require('cors');
